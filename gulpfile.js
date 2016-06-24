a089265a5ee05ee2946a8bb9eb44daff32aa4cf1
nitin <nitincse062@gmail.com>
2016-06-24 19:57:09 +0530

    Added gulp-coffee and tagline.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util')
+	coffee = require('gulp-coffee');
 
-gulp.task('log',function()
-{
-	gutil.log('Workflow are awesome');
+var coffeeSoucres = ['components/coffee/tagline.coffee'];
+
+gulp.task('coffee',function(){
+	gulp.src(coffeeSoucres)
+		.pipe(coffee({bare:true})).on('error',gutil.log)
+		.pipe(gulp.dest('components/scripts'));
 });
