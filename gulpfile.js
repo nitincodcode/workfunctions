728657a8d8db0d4976598872b3fdd5a24c36f040
nitin <nitincse062@gmail.com>
2016-06-24 20:06:23 +0530

    Added gulp concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util')
-	coffee = require('gulp-coffee');
+	coffee = require('gulp-coffee')
+	concat = require('gulp-concat');
 
 var coffeeSoucres = ['components/coffee/tagline.coffee'];
+var jsSoucres = ['components/scripts/rclick.js',
+				'components/scripts/tagline.js',
+				'components/scripts/pixgrid.js',
+				'components/scripts/template.js'];
 
 gulp.task('coffee',function(){
 	gulp.src(coffeeSoucres)
 		.pipe(coffee({bare:true})).on('error',gutil.log)
 		.pipe(gulp.dest('components/scripts'));
+});
+
+gulp.task('concat',function(){
+	gulp.src(jsSoucres)
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('bulid/development/js'));
 });
