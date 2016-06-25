f640d3eaeaa65861c953fa9706460833234b6887
nitin <nitincse062@gmail.com>
2016-06-26 00:12:27 +0530

    Added compass 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,37 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util')
 	coffee = require('gulp-coffee')
+	browserify = require('gulp-browserify')
+	compass = require('gulp-compass')
 	concat = require('gulp-concat');
 
-var coffeeSoucres = ['components/coffee/tagline.coffee'];
-var jsSoucres = ['components/scripts/rclick.js',
+var sassSources = ['components/sass/style.scss']
+var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources = ['components/scripts/rclick.js',
 				'components/scripts/tagline.js',
 				'components/scripts/pixgrid.js',
 				'components/scripts/template.js'];
 
 gulp.task('coffee',function(){
-	gulp.src(coffeeSoucres)
+	gulp.src(coffeeSources)
 		.pipe(coffee({bare:true})).on('error',gutil.log)
 		.pipe(gulp.dest('components/scripts'));
 });
 
 gulp.task('concat',function(){
-	gulp.src(jsSoucres)
+	gulp.src(jsSources)
 		.pipe(concat('script.js'))
+		.pipe(browserify())
 		.pipe(gulp.dest('bulid/development/js'));
+});
+
+gulp.task('compass',function(){
+	gulp.src(sassSources)
+		.pipe(compass({
+			sass:'components/sass',
+			image:'bulid/development/images',
+			style:'expanded'
+		}))
+		.on('error',gutil.log)
+		.pipe(gulp.dest('/bulid/development/css'));
 });
