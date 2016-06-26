9db5c7e93179036c382c2e97e83ae3980873fcfe
nitin <nitincse062@gmail.com>
2016-06-26 19:15:06 +0530

    'Added'

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
 	coffee = require('gulp-coffee')
 	browserify = require('gulp-browserify')
 	compass = require('gulp-compass')
+	connect = require('gulp-connect')
 	concat = require('gulp-concat');
 
 var sassSources = ['components/sass/style.scss']
@@ -22,8 +23,16 @@ gulp.task('concat',function(){
 	gulp.src(jsSources)
 		.pipe(concat('script.js'))
 		.pipe(browserify())
-		.pipe(gulp.dest('bulid/development/js'));
+		.pipe(gulp.dest('bulid/development/js'))
+		.pipe(connect.reload());
 });
+
+gulp.task('connect',function(){
+	connect.server({
+		root:'bulid/development/',
+		liveload:true
+	})
+	});
 
 gulp.task('compass',function(){
 	gulp.src(sassSources)
@@ -33,5 +42,14 @@ gulp.task('compass',function(){
 			style:'expanded'
 		}))
 		.on('error',gutil.log)
-		.pipe(gulp.dest('/bulid/development/css'));
+		.pipe(gulp.dest('/bulid/development/css'))
+		.pipe(connect.reload());
 });
+
+gulp.task('watch',function(){
+	gulp.watch('coffeeSources',[coffee]);
+	gulp.watch('jsSources',[concat]);
+	gulp.watch('components/sass/*.scss',[compass])
+})
+
+gulp.task('default',['coffee','compass','concat','connect','watch']);
